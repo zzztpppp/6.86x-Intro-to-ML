1226abac26ebd11ac66231417a1964eb88ca8368
zutianpei <cxztpcom@gmail.com>
2020-02-27 17:56:51 +0800

    Implement PCA feature reduction

--- mnist/part1/main.py
+++ mnist/part1/main.py
@@ -41,7 +41,7 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
 
 
 # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=0.01))
+# print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=0.01))
 
 
 #######################################################################
@@ -65,7 +65,7 @@ def run_svm_one_vs_rest_on_MNIST():
     return test_error
 
 
-print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
+# print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
 
 
 def run_multiclass_svm_on_MNIST():
@@ -81,7 +81,7 @@ def run_multiclass_svm_on_MNIST():
     return test_error
 
 
-print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
+# print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
 
 #######################################################################
 # 4. Multinomial (Softmax) Regression and Gradient Descent
@@ -114,10 +114,13 @@ def run_softmax_on_MNIST(temp_parameter=1):
 
     # TODO: add your code here for the "Using the Current Model" question in tab 4.
     #      and print the test_error_mod3
+
+
+    test_error = compute_test_error_mod3(test_x, test_y % 3, theta, temp_parameter)
     return test_error
 
 
-print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1))
+# print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1))
 
 # TODO: Find the error rate for temp_parameter = [.5, 1.0, 2.0]
 #      Remember to return the tempParameter to 1, and re-run run_softmax_on_MNIST
@@ -135,11 +138,17 @@ def run_softmax_on_MNIST_mod3(temp_parameter=1):
     See run_softmax_on_MNIST for more info.
     """
     # YOUR CODE HERE
-    raise NotImplementedError
+    train_x, train_y, test_x, test_y = get_MNIST_data()
+    train_y, test_y = update_y(train_y, test_y)
+    theta, cost_function_history = softmax_regression(train_x, train_y, temp_parameter, alpha=0.3, lambda_factor=1.0e-4, k=3, num_iterations=150)
+    plot_cost_function_over_time(cost_function_history)
+    test_error = compute_test_error(test_x, test_y, theta, temp_parameter)
+    # Save the model parameters theta obtained from calling softmax_regression to disk.
+    return test_error
 
 
 # TODO: Run run_softmax_on_MNIST_mod3(), report the error rate
-
+# print('softmax mod3 test_error=', run_softmax_on_MNIST_mod3(temp_parameter=1))
 
 #######################################################################
 # 7. Classification Using Manually Crafted Features
@@ -151,7 +160,7 @@ def run_softmax_on_MNIST_mod3(temp_parameter=1):
 
 
 n_components = 18
-pcs = principal_components(train_x)
+pcs = principal_components(center_data(train_x))
 train_pca = project_onto_PC(train_x, pcs, n_components)
 test_pca = project_onto_PC(test_x, pcs, n_components)
 # train_pca (and test_pca) is a representation of our training (and test) data
@@ -160,6 +169,19 @@ test_pca = project_onto_PC(test_x, pcs, n_components)
 
 # TODO: Train your softmax regression model using (train_pca, train_y)
 #       and evaluate its accuracy on (test_pca, test_y).
+def run_softmax_on_MNIST_pca(temp_parameter):
+
+    _, train_y, _, test_y = get_MNIST_data()
+    theta, cost_function_history = softmax_regression(train_pca, train_y, temp_parameter, alpha=0.3, lambda_factor=1.0e-4,
+                                                      k=10, num_iterations=150)
+    plot_cost_function_over_time(cost_function_history)
+    test_error = compute_test_error(test_pca, test_y, theta, temp_parameter)
+    # Save the model parameters theta obtained from calling softmax_regression to disk.
+    write_pickle_data(theta, "./theta.pkl.gz")
+
+    return test_error
+
+print("softmax pca test error:", run_softmax_on_MNIST_pca(1))
 
 
 # TODO: Use the plot_PC function in features.py to produce scatterplot
