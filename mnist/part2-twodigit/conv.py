43f09f174d69ab31308ea04042eafbb904747c12
Azu <cxztpcom@gmail.com>
2020-03-21 10:43:14 +0800

    Test accuracy 97.89, 97.61

--- mnist/part2-twodigit/conv.py
+++ mnist/part2-twodigit/conv.py
@@ -7,9 +7,9 @@ import utils_multiMNIST as U
 path_to_data_dir = '../Datasets/'
 use_mini_dataset = True
 
-batch_size = 64
+batch_size = 128
 nb_classes = 10
-nb_epoch = 40
+nb_epoch = 35
 num_classes = 10
 img_rows, img_cols = 42, 28 # input image dimensions
 
@@ -26,34 +26,50 @@ class CNN(nn.Module):
         # TODO initialize model layers here
         self.conv1 = nn.Conv2d(1, 16, (5, 5))
         self.pool1 = nn.MaxPool2d((2, 2))
-        self.conv2 = nn.Conv2d(16, 32, (5, 3))
-        self.pool2 = nn.MaxPool2d((2, 2))
-        self.conv3 = nn.Conv2d(32, 64, (3, 3))
-        self.pool3 = nn.MaxPool2d((2, 2))
+        self.conv2_1 = nn.Conv2d(16, 32, (3, 3))
+        self.pool2_1 = nn.MaxPool2d((2, 2))
+        self.conv2_2 = nn.Conv2d(16, 32, (3, 3))
+        self.pool2_2 = nn.MaxPool2d((2, 2))
 
-        self.flatten = Flatten()
-        self.linear = nn.Linear(1120, 256)
-        self.drop_out = nn.Dropout(0.4)
-        self.linear2 = nn.Linear(256, 128)
+        self.flatten1 = Flatten()
+        self.flatten2 = Flatten()
+        self.linear1_1 = nn.Linear(1280, 256)
+        self.linear1_2 = nn.Linear(1280,256)
+        self.drop_out1 = nn.Dropout(0.4)
+        self.drop_out2 = nn.Dropout(0.4)
+        self.linear2_1 = nn.Linear(256, 128)
+        self.linear2_2 = nn.Linear(256, 128)
         self.out1 = nn.Linear(128, 10)
-        self.out2 = nn.Linear(256, 10)
+        self.out2 = nn.Linear(128, 10)
     def forward(self, x):
         x.cuda(device)
-        x_conv1 = self.conv1(x)
-        x_relu1 = F.leaky_relu(x_conv1)
-        x_pooled1 = self.pool1(x_relu1)
-        x_conv2 = self.conv2(x_pooled1)
-        x_relu2 = F.leaky_relu(x_conv2)
-        x_pooled2 = self.pool2(x_relu2)
-        # x_conv3 = self.conv3(x_pooled2)
-        # x_relu3 = F.leaky_relu(x_conv3)
-        # x_pooled3 = self.pool3(x_relu3)
-        x_f = self.flatten(x_pooled2)
-        x_l = self.linear(x_f)
-        x_l_relu = self.drop_out(x_l)
-        x_l2 = self.linear2(x_l_relu)
-        x_l2_relu = F.leaky_relu(x_l2)
-        out_first_digit, out_second_digit = F.softmax(self.out1(x_l2_relu)), F.softmax(self.out2(x_l_relu))
+        x = self.conv1(x)
+        x = F.leaky_relu(x)
+        x = self.pool1(x)
+
+        # Apply different net branch for each digit
+        x_1 = self.conv2_1(x)
+        x_1 = F.leaky_relu(x_1)
+        x_1 = self.pool2_1(x_1)
+        x_1 = self.flatten1(x_1)
+        x_1 = self.linear1_1(x_1)
+        x_1 = F.leaky_relu(x_1)
+        x_1 = self.drop_out1(x_1)
+        x_1 = self.linear2_1(x_1)
+        x_1 = F.leaky_relu(x_1)
+
+        x_2 = self.conv2_2(x)
+        x_2 = F.leaky_relu(x_2)
+        x_2 = self.pool2_2(x_2)
+        x_2 = self.flatten2(x_2)
+        x_2 = self.linear1_2(x_2)
+        x_2 = F.leaky_relu(x_2)
+        x_2 = self.drop_out2(x_2)
+        x_2 = self.linear2_2(x_2)
+        x_2 = F.leaky_relu(x_2)
+
+        # Make prediction
+        out_first_digit, out_second_digit = F.softmax(self.out1(x_1)), F.softmax(self.out2(x_2))
 
         return out_first_digit, out_second_digit
 
