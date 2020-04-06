b8722e4fced08dddefd91a49dd0e915633b16bd1
Azu <cxztpcom@gmail.com>
2020-04-06 11:33:54 +0800

    Add code to run k-means with different initialization

--- netflix/main.py
+++ netflix/main.py
@@ -7,3 +7,14 @@ import em
 X = np.loadtxt("toy_data.txt")
 
 # TODO: Your code here
+for k in [1, 2, 3, 4]:
+    cost_list = []
+    for seed in [0,1, 2, 3, 4]:
+        gm, post = common.init(X, k, seed)
+        mixture, p, cost = kmeans.run(X, gm,post)
+        cost_list.append(cost)
+
+    print(min(cost_list))
+
+
+
